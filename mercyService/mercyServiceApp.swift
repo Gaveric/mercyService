aead83e54a5936219454776d90bc61ba91f813f8
Averic <aviekt@icloud.com>
2021-05-07 11:15:42 +0500

    Initial Commit

---
+++ mercyService/mercyServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  mercyServiceApp.swift
+//  mercyService
+//
+//  Created by User on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct mercyServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
